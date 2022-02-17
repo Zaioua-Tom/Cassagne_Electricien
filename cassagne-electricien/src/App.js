0e4f21220e7b9379c653e4ac6655253a1f2a0d7f
tomzaioua@gmail.com <tomzaioua@gmail.com>
2022-02-17 19:30:22 +0100

    first commit

---
+++ cassagne-electricien/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './images/logo/logo_officiel.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='logo'>
+        <img src={logo} alt="logo"/>
+        <h1>Test 2
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
